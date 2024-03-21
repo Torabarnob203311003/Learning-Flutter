79deac2865c0c5eb245ccbf0f3b7a393c8ca63fe
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-03-22 03:15:01 +0600

    drawer added

---
+++ lib/Home_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final int days = 60;
+  final String name = "with torab";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Cv App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("welcome flutter  $days challenge with $name"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:my_app/home_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
+  
+@override
   Widget build(BuildContext context) {
+    
     return  MaterialApp(
-      home: Container(
-      child: Text('welcome fluuter 30 days challenge'),
-    );
-
+      home: HomePage(),
       
     );
   }
