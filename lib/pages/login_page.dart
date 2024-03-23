852c9ea6ec055e749a95230a09d4d4ee83a0155b
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-03-23 17:39:12 +0600

    loginpage

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -6,11 +6,19 @@ class MyWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
+        color: Colors.white,
         child: Column(
-      children: [
-        Image.asset("assets/images/login_images.png",fit:BoxFit.cover,
-        ),
-      ],
-    ));
+          children: [
+            Image.asset(
+              "assets/images/login_images.png",
+              fit: BoxFit.cover,
+            ),
+            Text("welcome",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ))
+          ],
+        ));
   }
 }
