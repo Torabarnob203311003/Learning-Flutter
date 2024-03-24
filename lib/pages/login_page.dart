5b138f37054b534631aa2c6ca0de58c2fd212594
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-03-25 04:52:15 +0600

    day 3 completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -5,6 +5,7 @@ class MyWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var labelText = "username or email";
     return Material(
         color: Colors.white,
         child: Column(
@@ -13,11 +14,44 @@ class MyWidget extends StatelessWidget {
               "assets/images/login_images.png",
               fit: BoxFit.cover,
             ),
+            SizedBox(
+              height: 20.0,
+            ),
             Text("welcome",
                 style: TextStyle(
                   fontSize: 20,
                   fontWeight: FontWeight.bold,
-                ))
+                )),
+            SizedBox(
+              height: 20.0,
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.20),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                        hintText: "Username or Email",
+                        labelText: "Username or Email"),
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                        hintText: "Password", labelText: "Password"),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      print("yes u did it right");
+                    },
+                    child: Text("LogIn"),
+                    style: TextButton.styleFrom(),
+                  ),
+                ],
+              ),
+            )
           ],
         ));
   }
